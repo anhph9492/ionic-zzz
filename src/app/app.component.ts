c17718cb1204b99915db1e21f59501c3727b3dbf
osfpt_anhph <osfpt_anhph@ViettelGroup.ViettelAD.com>
2020-09-18 13:57:09 +0700

    fuck

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+
+import { AngularFireDatabase } from "@angular/fire/database";
+import { Subject, Observable, forkJoin, of } from "rxjs";
+import { takeUntil, map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private db: AngularFireDatabase
   ) {
     this.initializeApp();
+  }
+  table = "/student";
+  datas: any = [];
+  obsDatas: Observable<any>;
+  ngOnInit(): void {
+    this.obsDatas = this.db
+      .list(this.table)
+      .snapshotChanges()
+      .pipe(map((data) => data.map((a: any) => ({ ...a.payload.val() }))));
+  }
+
+  insert() {
+    let stu: any = {};
+    stu.name="hoang anh";
+    stu.address = "ha noi";
+    stu.email = "anhph9@fsoft.com.vn";
+    this.db.list(this.table).push(stu);
+  }
+
+  setData() {}
+
+  test() {
+    console.log(this.datas);
   }
 
   initializeApp() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,39 @@ import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireModule } from '@angular/fire';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBbot7j3dPF5hbfc2o5_qmjXS476oLlph8',
+  authDomain: 'banded-hexagon-275008.firebaseapp.com',
+  databaseURL: 'https://banded-hexagon-275008.firebaseio.com',
+  projectId: 'banded-hexagon-275008',
+  storageBucket: 'banded-hexagon-275008.appspot.com',
+  messagingSenderId: '1044881337458',
+  appId: '1:1044881337458:web:3cc3f6d2101bce1a6fa6eb',
+  measurementId: 'G-P6HRZWHB9V',
+};
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
-    BrowserModule, 
-    IonicModule.forRoot(), 
-    AppRoutingModule],
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+  ],
   providers: [
     // InAppBrowser,
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
